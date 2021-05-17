8ccf9cec9a1dd89499a73260bde893462a60a36a
Radeanu Andrei-Sergiu <andrei-sergiu.radeanu@student.tuiasi.ro>
2021-05-17 13:04:12 +0300

    -added frequency method and cleaned more

--- src/project/Game.java
+++ src/project/Game.java
@@ -1,4 +1,5 @@
 package project;
+
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -13,6 +14,7 @@ import java.util.Map;
 import java.util.Set;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
+
 /**
  * 
  * @author Bianca
@@ -22,87 +24,96 @@ import com.google.common.collect.Table;
  * Se initializeaza si creeaza jocul propriu zis
  */
 @SuppressWarnings({ "unused", "serial" })
-public class Game extends Canvas implements Runnable{
-/**
- * 	* @param WIDTH setam latimea
-	* @param HEIGHT setam inaltimea
-	* @param title setam numele ferestrei
-	* @param thread fir de executie
-	* @param isRunning spune daca jocul este pornit sau oprit
-	* @param handler primeste informatii despre jucator
-	* @param input primeste informatii de la tastatura
-	* @param level
- */
-	public static int WIDTH=815,HEIGHT=560;
-	public String title="Pacman";
+public class Game extends Canvas implements Runnable {
+	/**
+	 * * @param WIDTH setam latimea
+	 * 
+	 * @param HEIGHT
+	 *            setam inaltimea
+	 * @param title
+	 *            setam numele ferestrei
+	 * @param thread
+	 *            fir de executie
+	 * @param isRunning
+	 *            spune daca jocul este pornit sau oprit
+	 * @param handler
+	 *            primeste informatii despre jucator
+	 * @param input
+	 *            primeste informatii de la tastatura
+	 * @param level
+	 */
+	public static int WIDTH = 815, HEIGHT = 560;
+	public String title = "Pacman";
 	private Thread thread;
 	public boolean isRunning = false;
 
-	//Instances
+	// Instances
 	private Handler handler;
 	private KeyInput input;
 	private BufferedImage level = null;
-	private Table<Integer, Integer, Integer> frequency;
-	
+	private Table<Integer, Integer, Integer> frequency = HashBasedTable
+			.create();
+	int initY = 32;
+	int initX = 32;
+
 	/**
-	 * Costructor Game
-	 * Creeaza o noua fereastra prin apelul constructorului Window
+	 * Costructor Game Creeaza o noua fereastra prin apelul constructorului
+	 * Window
 	 */
-	public Game(){
-		new Window(WIDTH,HEIGHT,title,this);
+	public Game() {
+		new Window(WIDTH, HEIGHT, title, this);
 		start();
 		init();
-		//
-		handler.addObject(new Pacman(31, 31, ID.Pacman,input,handler));
-		// handler.addObject(new Frame(37,40,ID.Wall));
-		
+
+		handler.addObject(new Pacman(initX, initY, ID.Pacman, input, handler));
 		BufferedImageLoader loader = new BufferedImageLoader();
 		level = loader.loadImage("/map/mapfinal.png");
 		loadLevel(level);
-		//MovementFromFile();
-		
+
+		// MovementFromFile();
+
 	}
-	
-	public void MovementFromFile(){
-		
+
+	public void MovementFromFile() {
 		BufferedReader reader;
-		try{
-			reader = new BufferedReader(new FileReader("E:/Work/Java/Pacman-PipPR/src/movement/movement.txt"));
+		try {
+			reader = new BufferedReader(new FileReader(
+					"src/movement/movement.txt"));
 			String line = reader.readLine();
-			while(line != null){
-				switch(line){
+			while (line != null) {
+				switch (line) {
 				case "up":
-					input.keys[2]=true;
-					
+					input.keys[2] = true;
+
 					try {
-						TimeUnit.SECONDS.sleep(1);
+						TimeUnit.MILLISECONDS.sleep(300);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
 					break;
 				case "down":
-					input.keys[3]=true;
-						
+					input.keys[3] = true;
+
 					try {
-						TimeUnit.SECONDS.sleep(1);
+						TimeUnit.MILLISECONDS.sleep(300);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
 					break;
 				case "left":
-					input.keys[1]=true;
-						
+					input.keys[1] = true;
+
 					try {
-						TimeUnit.SECONDS.sleep(1);
+						TimeUnit.MILLISECONDS.sleep(300);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
 					break;
 				case "right":
-					input.keys[0]=true;
-						
+					input.keys[0] = true;
+
 					try {
-						TimeUnit.SECONDS.sleep(1);
+						TimeUnit.MILLISECONDS.sleep(300);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
@@ -110,39 +121,41 @@ public class Game extends Canvas implements Runnable{
 				default:
 					System.out.println("wrong line");
 				}
-				line=reader.readLine();
+				line = reader.readLine();
 			}
 			reader.close();
-		}catch(IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private void init(){
+
+	private void init() {
 		handler = new Handler();
 		input = new KeyInput();
 		this.addKeyListener(input);
 	}
-	
+
 	/**
 	 * Verifica starea in care se afla jocul, daca ruleaza sau nu
 	 * 
 	 */
-	private synchronized void start(){
-		if(isRunning) return;
-		
+	private synchronized void start() {
+		if (isRunning)
+			return;
+
 		thread = new Thread(this);
 		thread.start();
 		isRunning = true;
 	}
-	
+
 	/**
 	 * Verifica starea in care se afla jocul, daca ruleaza sau nu
 	 * 
 	 */
-	private synchronized void stop(){
-		if(!isRunning) return; 
-		
+	private synchronized void stop() {
+		if (!isRunning)
+			return;
+
 		try {
 			thread.join();
 		} catch (InterruptedException e) {
@@ -151,150 +164,168 @@ public class Game extends Canvas implements Runnable{
 		}
 		isRunning = false;
 	}
-	
-	
-	//gameloop
-	public void run(){
+
+	// gameloop
+	public void run() {
 		this.requestFocus();
 		long lastTime = System.nanoTime();
-		double amountOfTicks = 60.0;
+		double amountOfTicks = 30.0;
 		double ns = 1000000000 / amountOfTicks;
 		double delta = 0;
 		long timer = System.currentTimeMillis();
 		int updates = 0;
 		int frames = 0;
-		while(isRunning){
+		while (isRunning) {
 			long now = System.nanoTime();
 			delta += (now - lastTime) / ns;
 			lastTime = now;
-			while(delta >= 1){
+			while (delta >= 1) {
 				tick();
 				updates++;
 				delta--;
 			}
 			render();
 			frames++;
-					
-			if(System.currentTimeMillis() - timer > 1000){
+
+			if (System.currentTimeMillis() - timer > 1000) {
 				timer += 1000;
-				System.out.println("FPS: " + frames + " TICKS: " + updates);
+				// System.out.println("FPS: " + frames + " TICKS: " + updates);
 				frames = 0;
 				updates = 0;
+
+			}
+
+			GameObject pacObject = handler.object.get(0);
+
+			if (initY != (int) pacObject.y || initX != (int) pacObject.x) {
+				checkFrequency(initX, initY);
+				initX = (int) pacObject.x;
+				initY = (int) pacObject.y;
 			}
 		}
 		stop();
 	}
-	
+
 	/**
-	 * tick() si render() se ocupa de functionalitatea jocului
-	 * Fac ca jocul sa nu se intrerupa
+	 * tick() si render() se ocupa de functionalitatea jocului Fac ca jocul sa
+	 * nu se intrerupa
 	 */
-	private void tick(){
-		//Updates the game
+	private void tick() {
+		// Updates the game
 		handler.tick();
+
 	}
-	
-	private void render(){
-		//Renders the game
-		BufferStrategy bs= this.getBufferStrategy();
-		if(bs==null){
+
+	private void render() {
+		// Renders the game
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
 			this.createBufferStrategy(3);
 			return;
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
-		
-		//Our rendering
+
+		// Our rendering
 		g.setColor(Color.black);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
-		
+
 		handler.render(g);
-		
+
 		bs.show();
 		g.dispose();
 	}
-	
-	
-	/** 
+
+	/**
 	 * @param image
 	 */
-	private void loadLevel(BufferedImage image){
-		int w=image.getWidth();
-		int h=image.getHeight();
-		
-		System.out.println("w:"+ w +"h:"+ h);
-		
-	    int mat[][] = new int [h][w];{
-	    for(int i=0; i<h;i++){
-	       for(int j=0; j<w;j++){
-	            mat[i][j]=0;
-	        }}}
-	    
-	    Table<Integer, Integer, Integer> frequency = HashBasedTable.create();
-		
-		for(int xx=0;xx<w;xx++){
-			for(int yy=0;yy<h;yy++){
-				int pixel=image.getRGB(xx, yy);
-				int red = (pixel>>16) & 0xff;
-				int green = (pixel>>8) & 0xff;
-				int blue= (pixel) & 0xff;
-				
-				if(blue==255){
-					handler.addObject(new Tile(xx*32,yy*32,ID.Wall));
-					mat[yy][xx]=0;}
-				if(green==255){
-					handler.addObject(new Ghost(xx*32,yy*32,ID.Ghost));
-					mat[yy][xx]=9;
-				}
-				if(red==255){
-					mat[yy][xx]=1;
-					frequency.put(yy,xx, 1);
-					handler.addObject(new Walk(xx*32,yy*32,ID.Walk,handler,input,Color.black,frequency));
-					
-				}
-				
+	private void loadLevel(BufferedImage image) {
+		int w = image.getWidth();
+		int h = image.getHeight();
+		int mat[][] = new int[h][w];
+		System.out.println("w:" + w + "h:" + h);
+
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				mat[i][j] = 0;
 			}
 		}
-		for(int i=0; i<h;i++){
-		       for(int j=0; j<w;j++){
-		            System.out.print(mat[i][j]);
-		        }
-		       System.out.println("");
-		       }
-		
-		
-		
-	}
-	
-	public void checkfreq() {
 
-		for (int i = 0; i < handler.object.size(); i++) {
+		for (int xx = 0; xx < w; xx++) {
+			for (int yy = 0; yy < h; yy++) {
+				int pixel = image.getRGB(xx, yy);
+				int red = (pixel >> 16) & 0xff;
+				int green = (pixel >> 8) & 0xff;
+				int blue = (pixel) & 0xff;
+
+				if (blue == 255) {
+					handler.addObject(new Tile(xx * 32, yy * 32, ID.Wall));
+					mat[yy][xx] = 0;
+				}
+				if (green == 255) {
+					handler.addObject(new Ghost(xx * 32, yy * 32, ID.Ghost));
+					frequency.put(yy * 32, xx * 32, 1);
+					mat[yy][xx] = 9;
+				}
+				if (red == 255) {
+					mat[yy][xx] = 1;
+					frequency.put(yy * 32, xx * 32, 1);
+					handler.addObject(new Walk(xx * 32, yy * 32, ID.Walk,
+							Color.BLACK));
+
+				}
+
+			}
+		}
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				System.out.print(mat[i][j]);
+			}
+			System.out.println("");
+		}
+
+	}
+
+	public void checkFrequency(int x, int y) {
+		GameObject pacObject = handler.object.get(0);
+
+		for (int i = 1; i < handler.object.size(); i++) {
 			GameObject tempObject = handler.object.get(i);
-			if (tempObject.getId() == ID.Pacman) {
-				if (tempObject.getBounds().intersects(tempObject.getBounds())) {
-					switch (frequency.get((int) y, (int) x)) {
+			if (tempObject.getId() == ID.Walk) {
+				if (tempObject.getBounds().intersects(pacObject.getBounds())) {
+					switch (frequency.get(y, x)) {
 					case 1:
-						new Walk(x * 32, y * 32, ID.Walk, handler, input, Color.white, frequency);
-						break;
+						handler.addObject(new Walk(x, y, ID.Walk, Color.WHITE));
+						frequency.put(y, x, frequency.get(y, x) + 1);
+						return;
 					case 2:
-						new Walk(x * 32, y * 32, ID.Walk, handler, input, Color.red, frequency);
-						break;
+						handler.addObject(new Walk(x, y, ID.Walk, Color.ORANGE));
+						frequency.put(y, x, frequency.get(y, x) + 1);
+						return;
+					case 3:
+						handler.addObject(new Walk(x, y, ID.Walk, Color.RED));
+						frequency.put(y, x, frequency.get(y, x) + 1);
+						return;
+					case 4:
+						handler.addObject(new Walk(x, y, ID.Walk, Color.MAGENTA));
+						frequency.put(y, x, frequency.get(y, x) + 1);
+						return;
 					default:
+						handler.addObject(new Walk(x, y, ID.Walk, Color.GREEN));
 						System.out.println("default");
+						return;
 					}
-					frequency.put((int) y, (int) x, frequency.get((int) y, (int) x) + 1);
+					// frequency.put(y, x, frequency.get(y, x) + 1);
+
 				}
 			}
-
 		}
-
 	}
-	
-	
+
 	/**
 	 * Cream o noua instanta de Game
 	 */
-	public static void main(String args[]){
+	public static void main(String args[]) {
 		new Game();
 	}
 
--- src/project/Walk.java
+++ src/project/Walk.java
@@ -3,62 +3,44 @@ package project;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+/**
+ * 
+ * @author Bianca
+ *
+ */
+
+public class Walk extends GameObject {
 	/**
 	 * 
-	 * @author Bianca
-	 *
+	 * @param x
+	 *            axa X pe care se afla obiectul
+	 * @param y
+	 *            axa Y pe care se afla obiectul
+	 * @param id
+	 *            identificatorul pentru personaj
 	 */
+	private Color color;
 
-import com.google.common.collect.Table;
+	public Walk(float x, float y, ID id,Color color) {
+		super(x, y, id);
+		this.color = color;
+	}
 
+	public void render(Graphics g) {
 
-	/**
-	 * 
-	 * Clasa care creeaza inamicul
-	 *
-	 */
+		g.setColor(color);
+		g.drawRect((int) x, (int) y, 32, 32);
 
-	
-	public class Walk  extends GameObject  {
-		/**
-		 * 
-		 * @param x axa X pe care se afla obiectul
-		 * @param y axa Y pe care se afla obiectul
-		 * @param id identificatorul pentru personaj
-		 */
-		private KeyInput input;
-		private Handler handler;
-		private Color color; 
+	}
+
+	@Override
+	public void tick() {
 		
-		 
-		public  Walk (float x, float y, ID id,Handler handler,KeyInput input, Color color, Table<Integer, Integer, Integer> frequency ) {
-			super(x, y, id);
-			this.handler = handler;
-			this.input = input;
-			this.color = color;
-			this.frequency= frequency;
-		}
+	}
 
-		public void render(Graphics g) {
-			
-			g.setColor(color);
-			g.fillRect((int)x, (int)y, 32, 32);
-			
-			}
+	@Override
+	public Rectangle getBounds() {
+		return new Rectangle((int) x, (int) y, 32, 32);
+	}
 
-		@Override
-		public void tick() {
-			
-			if (input.keys[0]||input.keys[1]||input.keys[2]||input.keys[3])
-					//checkfreq();
-			
-		}
-
-		@Override
-		public Rectangle getBounds() {
-			return new Rectangle((int)x,(int)y,32,32);
-		}
-	
-
-	
-			}	
+}
