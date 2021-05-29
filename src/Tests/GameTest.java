37f31566594317d5c64abac916c9b36f79798299
Andrei-Antonio Robu <robu.andrew@gmail.com>
2021-05-29 20:01:45 +0300

    updated GameTest.java

--- src/Tests/GameTest.java
+++ src/Tests/GameTest.java
@@ -1,35 +1,83 @@
 package Tests;
 
-
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import project.BufferedImageLoader;
 import project.Game;
+import project.Handler;
+import project.KeyInput;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
-class GameTest {
-
+public class GameTest {
     private Game game;
+    private BufferedImage level = null;
+    public static int WIDTH = 815, HEIGHT = 532;
+    public String title = "Pacman";
+    private Handler handler;
+    private KeyInput input;
 
     @BeforeEach
     void setUp() throws Exception {
         game = new Game();
     }
 
-    @AfterEach
-    void tearDown() {
-
-    }
-
     @Test
-    void movementFromFile() {
-        //TODO if throws exception
-    }
-
-    @Test
-    public void testrun() {
+    public void runningTest() {
         assertTrue(game.isRunning, "the game is running");
     }
+
+    @Test
+    public void loadMapTest() {
+        BufferedImageLoader loader = new BufferedImageLoader();
+        level = loader.loadImage("/map/mapfinal.png");
+        assertNotNull(level, "Map load complete");
+    }
+
+    @Test
+    public void movementFromFileTest() {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(
+                    "src/movement/movement.txt"));
+            String line = reader.readLine();
+            assertNotNull(line, "Reading from file complete");
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    @Test
+    public void handlerTest() {
+        handler = new Handler();
+        input = new KeyInput();
+        game.addKeyListener(input);
+        assertNotNull(input);
+    }
+
+    @Test
+    public void windowWidthTest() {
+        assertEquals(WIDTH, game.getWidth());
+    }
+    @Test
+    public void windowHeightTest() {
+        assertEquals(HEIGHT,game.getHeight());
+    }
+
+    @Test
+    public void windowTitleTest() {
+        assertEquals(title,game.getName());
+    }
+
+
+
+
 }
